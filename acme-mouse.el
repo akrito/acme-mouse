eb742a589bbab8332c809b96acb827dffdbc6617
Alex Kritikos <alex@archbook.localdomain>
2009-04-29 22:03:19 -0500

    Updated comments to show fixed issues

--- acme-mouse.el
+++ acme-mouse.el
@@ -33,10 +33,10 @@
 
 ;; Currently acme-search acts like * in vim. We should rewrite it to
 ;; act more like Acme:
-;;   If there's a region, match against it
-;;   If not, match against the word under the cursor
+;;   If there's a region, match against it - done
+;;   If not, match against the word under the cursor - done
 ;;   If a file matches the text, open or switch to it in a new window
-;;   Else, search through the file, wrapping at the bottom
+;;   Else, search through the file, wrapping at the bottom - done
 ;;   Allow drag-highlighting with the right button (secondary selection?)
 
 ;; Make this a proper minor mode without clobbering keymaps and global
@@ -46,9 +46,6 @@
 
 ;; Sometimes the region is active after the command, when we don't
 ;; want it to be. Leads to large highlighted regions
-
-;; Cutting or Pasting, then moving the arrow keys extends the
-;; region. Moving the arrow keys should remove the region
 
 ;; Double-click, then mouse 3 pastes instead of searches
 
